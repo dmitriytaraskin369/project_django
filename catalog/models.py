9a2ff64afda2f9010135306eba707fed6b5612b7
Dmitriy Taraskin <dmit.taraskin@gmail.com>
2024-01-17 17:47:52 +0500

    update 2.0

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=250, verbose_name='название продукта')
+    description = models.TextField(verbose_name='описание продукта')
+    image = models.ImageField(upload_to='products_images/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, **NULLABLE, verbose_name='категория продукта')
+    price = models.IntegerField(verbose_name='цена за 1шт. товара')
+    date_of_creation = models.DateField()
+    date_of_change = models.DateField()
+
+    def __str__(self):
+        return f'{self.title} {self.price}'
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=250, verbose_name='название категории')
+    description = models.TextField(verbose_name='описание категории')
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, **NULLABLE, verbose_name='вышестоящая категория')
